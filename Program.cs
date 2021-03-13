0c96607d76059224b8a2a42d12d205eace4f77c2
Patricio-B <Rumiman2.0@gmail.com>
2021-03-12 22:56:55 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fname = "Patricio";
+            string lname = "Bayas";
+            Console.WriteLine("Hello my name is {0} {1}", fname, lname);
+        }
+    }
+}
